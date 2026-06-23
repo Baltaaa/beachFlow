25fc8fcdd24679842ed5b3f31a94f34fad290be4
[dyad] <baltasarbruschetti@yahoo.com.ar>
2026-06-23 20:56:09 -0300

    [dyad] Ajustar el logo a factor de escala x1.3 - wrote 1 file(s)

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -46,7 +46,7 @@ export default function Navbar() {
         isScrolled ? 'h-16' : 'h-20'
       }`}>
         
-        {/* LEFT: Logo escalado x1.5 */}
+        {/* LEFT: Logo escalado x1.3 */}
         <button 
           onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })} 
           className="flex items-center cursor-pointer justify-self-start transition-all duration-300 hover:opacity-90"
@@ -55,7 +55,7 @@ export default function Navbar() {
             src="/logo-prius.png" 
             alt="Prius" 
             className={`w-auto object-contain transition-all duration-500 ${
-              isScrolled ? 'h-[66px] md:h-[72px]' : 'h-[96px] md:h-[108px]'
+              isScrolled ? 'h-[57px] md:h-[62px]' : 'h-[83px] md:h-[94px]'
             }`}
           />
         </button>
